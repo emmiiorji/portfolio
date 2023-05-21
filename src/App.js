6460007fb829d3d8831da733ebb9a84d99fce40f
emmiiorji <emmanuelorji2013@gmail.com>
2023-05-21 11:29:05 +0100

    Use Header and Headline in App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import Header from './components/Header';
+import Headline from './components/Headline';
 
 function App() {
   return (
-    <></>
+    <>
+      <Header />
+      <Headline />
+    </>
   );
 }
 
