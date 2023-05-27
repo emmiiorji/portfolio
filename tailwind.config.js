8cc9181280d2937e0c3a7efd8d85d72c0d2f3068
emmiiorji <emmanuelorji2013@gmail.com>
2023-05-27 08:35:23 +0100

    Add custom colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customBlack: '#1c1a19',
+        customOrange: 'ff6b00',
+        customOffWhite: '#fff5e1',
+        customGrey: '#3c3a39',
+        customLightGray: '#dbd8d7',
+      },
+    },
   },
   plugins: [],
 };
