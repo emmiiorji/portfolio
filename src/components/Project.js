da19b87d2769a135e7997bcccb7254c352067373
emmiiorji <emmanuelorji2013@gmail.com>
2023-06-19 11:51:13 +0100

    Add proptypes for project prop in Project component

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,4 +4,14 @@ import Languages from './Languages';
 
 const Project = () => {};
 
+Project.propTypes = {
+  project: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    languages: PropTypes.arrayOf(PropTypes.string).isRequired,
+    imgMobile: PropTypes.string,
+    imageURL: PropTypes.string,
+  }).isRequired,
+};
+
 export default Project;
