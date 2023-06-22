a497ba6eff05dd87b93d69483bb0f534d9f3b8aa
emmiiorji <emmanuelorji2013@gmail.com>
2023-06-22 13:18:31 +0100

    Initialize state to hold the id of modal to be displayed

---
+++ src/components/Projects.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
+import Project from './Project';
+import projects from '../helpers/projectsData';
+import Modal from './Modal';
+import LeadingProject from './LeadingProject';
+
+const Projects = () => {
+  const [modalProjectId, setModalProjectId] = useState(null);
+
+  return <></>;
+};
+
+export default Projects;
