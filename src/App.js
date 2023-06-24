a7ae4ddf14f4d5909a3e27261005733c659de68e
emmiiorji <emmanuelorji2013@gmail.com>
2023-06-24 13:52:53 +0100

    Render Projects component in the App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Headline from './components/Headline';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <>
       <Header />
       <Headline />
+      <Projects />
     </>
   );
 }
