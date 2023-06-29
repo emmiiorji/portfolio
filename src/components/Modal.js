2293d75bfbd1f1f1e48133c30bb4664e92fb2265
emmiiorji <emmanuelorji2013@gmail.com>
2023-06-29 11:19:12 +0100

    Remove closeModal onClick listener on modal container

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -13,7 +13,7 @@ const Modal = ({ projectId, show, closeModal }) => {
     <div className="project-modal" style={{ display: `${show ? 'block' : 'none'}` }}>
       {/* eslint-disable-next-line max-len */}
       {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events */}
-      <div className="project-margin" onClick={() => closeModal()}>
+      <div className="project-margin">
         <div className="project-modal-container">
           <div className="modal-header">
             <h2>{name}</h2>
