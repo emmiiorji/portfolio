48ac5fcbe984622963cf036b4ef9d815f81fc18d
emmiiorji <emmanuelorji2013@gmail.com>
2023-07-04 17:35:18 +0100

    Create 404 page

---
+++ src/routes/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div className="not-found">
+    <h1>404</h1>
+    <h2>Page Not Found</h2>
+    <p>
+      The page you are looking for might have been removed, had its name
+      changed or is temporarily unavailable.
+    </p>
+  </div>
+);
+
+export default NotFound;
