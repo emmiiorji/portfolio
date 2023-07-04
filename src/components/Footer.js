6288b4a782561eb0962ffc5a7e8e3a130f07035d
emmiiorji <emmanuelorji2013@gmail.com>
2023-07-04 17:34:54 +0100

    Separate the footer section

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Socials from './Socials';
+
+const Footer = () => (
+  <footer className="footer-container">
+    <hr id="footer-divide" />
+    <Socials />
+    <hr id="footer-bottom" />
+  </footer>
+);
+
+export default Footer;
