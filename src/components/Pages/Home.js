4a1efb4e69f705c31a4ba35285cc25e5b9e4c966
emmiiorji <emmanuelorji2013@gmail.com>
2023-07-04 17:35:37 +0100

    Create Home page

--- src/components/Pages/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Home = () => (
-  <>
-    <></>
-  </>
-);
-
-export default Home;
---
+++ src/routes/Home.js
@@ -0,0 +1,16 @@
+import '../App.css';
+import About from '../components/About';
+import Contact from '../components/Contact';
+import Headline from '../components/Headline';
+import Projects from '../components/Projects';
+
+const Home = () => (
+  <>
+    <Headline />
+    <Projects />
+    <About />
+    <Contact />
+  </>
+);
+
+export default Home;
