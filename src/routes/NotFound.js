1ade1d7b2a4f7eac76e15a0518c83a414e481c93
emmiiorji <emmanuelorji2013@gmail.com>
2023-07-05 08:59:49 +0100

    Complete the 404 route component

--- src/routes/NotFound.js
+++ src/routes/NotFound.js
@@ -1,14 +1,72 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import './NotFound.scss';
+import { Link } from 'react-router-dom';
 
-const NotFound = () => (
-  <div className="not-found">
-    <h1>404</h1>
-    <h2>Page Not Found</h2>
-    <p>
-      The page you are looking for might have been removed, had its name
-      changed or is temporarily unavailable.
-    </p>
-  </div>
-);
+const NotFound = () => {
+  useEffect(() => {
+    const pageX = document.documentElement.clientWidth;
+    const pageY = document.documentElement.clientHeight;
+    let mouseY = 0;
+    let mouseX = 0;
+
+    const moveGhostEyes = (event) => {
+      // verticalAxis
+      mouseY = event.pageY;
+      const yAxis = ((pageY / 2 - mouseY) / pageY) * 300;
+      // horizontalAxis
+      mouseX = event.pageX / -pageX;
+      const xAxis = -mouseX * 100 - 100;
+
+      document.querySelector('.box__ghost-eyes').style.transform = `translate(${xAxis}%, -${yAxis}%)`;
+
+      // console.log('X: ' + xAxis);
+    };
+    document.addEventListener('mousemove', moveGhostEyes);
+
+    return () => {
+      document.removeEventListener('mousemove', moveGhostEyes);
+    };
+  }, []);
+
+  return (
+    <main className="notfound">
+      <div className="box">
+        <div className="box__ghost">
+          <div className="symbol" />
+          <div className="symbol" />
+          <div className="symbol" />
+          <div className="symbol" />
+          <div className="symbol" />
+          <div className="symbol" />
+          <div className="box__ghost-container">
+            <div className="box__ghost-eyes">
+              <div className="box__eye-left" />
+              <div className="box__eye-right" />
+            </div>
+            <div className="box__ghost-bottom">
+              <div />
+              <div />
+              <div />
+              <div />
+              <div />
+            </div>
+          </div>
+          <div className="box__ghost-shadow" />
+        </div>
+
+        <div className="box__description">
+          <div className="box__description-container">
+            <div className="box__description-title">Whoops!</div>
+            <div className="box__description-text">It seems that the page you&#39;re looking for could not be found</div>
+          </div>
+
+          <Link to="/" className="box__button" rel="noreferrer">Go Home</Link>
+
+        </div>
+
+      </div>
+    </main>
+  );
+};
 
 export default NotFound;
