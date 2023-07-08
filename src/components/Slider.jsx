127c221f13d1635fba0aef8997e9515c7a97d0a7
emmiiorji <emmanuelorji2013@gmail.com>
2023-07-08 11:26:45 +0100

    Update Slider component

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import PrevArrow from './PrevArrow';
+import NextArrow from './NextArrow';
 
-const InvestigatorsSlider = () => <div />;
+const InvestigatorsSlider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  return (
+    <div className="slider-container">
+      <Slider
+        prevArrow={<PrevArrow disabled={currentSlide === 0} />}
+        nextArrow={<NextArrow disabled={currentSlide === 2} />}
+        beforeChange={(current, next) => setCurrentSlide(next)}
+        className="slider"
+        slidesToShow={3}
+      />
+    </div>
+  );
+};
 
 export default InvestigatorsSlider;
