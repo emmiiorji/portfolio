6a0b14d8e7a8bddba6799ffb793ed5057f98d7c6
emmiiorji <emmanuelorji2013@gmail.com>
2023-07-10 13:01:05 +0100

    Use react-icons for hamburger

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AiOutlineClose } from 'react-icons/ai';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { BsFillEnvelopeFill } from 'react-icons/bs';
 
-const Header = () => (
-  <header>
-    <nav className="toolbar">
-      <a href="/" id="author-name">Emmanuel Orji</a>
-      <div id="mobile-menu">
-        <i className="fa-solid fa-bars" id="hamburger" />
-        <i className="fas fa-times close" />
-      </div>
-      <ul id="nav-menu">
-        <li><a href="#my-works">Portfolio</a></li>
-        <li><a href="#about-me">About</a></li>
-        <li><a href="#contact-me">Contact</a></li>
-        <li><i className="fa-regular fa-envelope" /></li>
-      </ul>
-    </nav>
-  </header>
-);
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState('');
+
+  return (
+    <header>
+      <nav className={`toolbar ${menuOpen ? 'show-mobile-menu' : ''}`}>
+        <a href="/" id="author-name">Emmanuel Orji</a>
+        <div
+          id="mobile-menu"
+          tabIndex={0}
+          role="button"
+          onKeyDown={toggleMenu}
+        >
+          <GiHamburgerMenu
+            id="hamburger"
+            style={{ display: `${menuOpen ? 'none' : 'inline'}` }}
+            onClick={(event) => { if (event.key === 'Enter') toggleMenu(); }}
+          />
+          <AiOutlineClose
+            className="close"
+            style={{ display: `${menuOpen ? 'inline' : 'none'}` }}
+            onClick={toggleMenu}
+          />
+        </div>
+        <ul id="nav-menu">
+          <li><a href="#my-works" onClick={toggleMenu}>Portfolio</a></li>
+          <li><a href="#about-me" onClick={toggleMenu}>About</a></li>
+          <li><a href="#contact-me" onClick={toggleMenu}>Contact</a></li>
+          <li><BsFillEnvelopeFill /></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
