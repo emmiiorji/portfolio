9590e4d14c6de1ca1bb13be596a04d50847a5f2c
emmiiorji <emmanuelorji2013@gmail.com>
2023-07-14 15:31:29 +0100

    Use tailwind classes for styling mobile menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,19 @@ import { BsFillEnvelopeFill } from 'react-icons/bs';
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [menuClosing, setMenuClosing] = useState(false);
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
-  const toggleMenu = () => setMenuOpen(!menuOpen);
+  const toggleMenu = (event, menuClosing) => {
+    if (menuClosing) {
+      setMenuClosing(true);
+      setTimeout(() => {
+        setMenuOpen(!menuOpen);
+        setMenuClosing(false);
+      }, 800); // Waiting for closing menu animation to finish
+    } else {
+      setMenuOpen(!menuOpen);
+    }
+  };
 
   useEffect(() => {
     window.addEventListener('resize', () => {
@@ -15,29 +26,36 @@ const Header = () => {
   }, []);
 
   return (
-    <header>
+    <header className="font-roboto">
       <nav
-        className={`toolbar ${menuOpen ? 'show-mobile-menu' : ''}`}
-        style={{ animation: `${menuOpen ? 'slideIn 1s ease-in-out' : ''}` }}
+        className={`flex ${menuOpen ? 'animate-slideIn flex-col bg-customGrey h-screen w-screen absolute ' : 'justify-between'} ${`${menuClosing ? 'animate-slideOut' : ''}`} py-2.5 px-3 bg-customGrey h-12 text-white toolbar`}
       >
-        <a href="/" id="author-name">Emmanuel Orji</a>
-        <div id="mobile-menu">
+        <a className={`${menuOpen ? 'hidden' : ''}`} href="/" id="author-name">Emmanuel Orji</a>
+        <div className={`mr-3 flex items-center ${menuOpen ? 'self-end ' : ''}`} id="mobile-menu">
           <GiHamburgerMenu
             id="hamburger"
-            style={{ display: `${!menuOpen && isMobile ? 'inline' : 'none'}` }}
+            className={`${!menuOpen && isMobile ? '' : 'hidden'}`}
             onClick={toggleMenu}
           />
           <AiOutlineClose
-            className="close"
-            style={{ display: `${menuOpen ? 'inline' : 'none'}` }}
-            onClick={toggleMenu}
+            className={`${!menuOpen ? 'hidden' : ''}`}
+            onClick={(event) => toggleMenu(event, true)}
           />
         </div>
-        <ul id="nav-menu">
-          <li><a href="#my-works" onClick={toggleMenu}>Portfolio</a></li>
-          <li><a href="#about-me" onClick={toggleMenu}>About</a></li>
-          <li><a href="#contact-me" onClick={toggleMenu}>Contact</a></li>
-          <li><BsFillEnvelopeFill className="fa-envelope" /></li>
+        <ul
+          className={`px-3 py-2.5 flex gap-4 ${menuOpen ? 'text-customMilk text-3xl/[44px] font-bold flex-col' : `${isMobile ? 'hidden' : 'font-semibold items-center'}`}`}
+          id="nav-menu"
+        >
+          <li className={`${menuOpen ? 'border-b border-customLighterGray' : ''}`}>
+            <a href="#my-works" className={`${menuOpen ? 'p-4' : ''}`} onClick={(event) => toggleMenu(event, true)}>Portfolio</a>
+          </li>
+          <li className={`${menuOpen ? 'border-b border-customLighterGray' : ''}`}>
+            <a href="#about-me" className={`${menuOpen ? 'p-4' : ''}`} onClick={(event) => toggleMenu(event, true)}>About</a>
+          </li>
+          <li className={`${menuOpen ? 'border-b border-customLighterGray' : ''}`}>
+            <a href="#contact-me" className={`${menuOpen ? 'p-4' : ''}`} onClick={(event) => toggleMenu(event, true)}>Contact</a>
+          </li>
+          <li><BsFillEnvelopeFill className={`${menuOpen ? 'hidden' : ''}`} /></li>
         </ul>
       </nav>
     </header>
