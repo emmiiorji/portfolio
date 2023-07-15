687b4d593bcb3a8f7ca86ebc057255e20d5df1bc
emmiiorji <emmanuelorji2013@gmail.com>
2023-07-15 09:12:01 +0100

    Fix error in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
     extend: {
       colors: {
         customBlack: '#1c1a19',
-        customOrange: 'ff6b00',
+        customOrange: '#ff6b00',
         customOffWhite: '#fff5e1',
         customGrey: '#3c3a39',
         customLightGray: '#dbd8d7',
