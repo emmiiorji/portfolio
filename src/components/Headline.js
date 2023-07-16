1edf291e9d21af7093805b551ee4d653fe2e6ddb
emmiiorji <emmanuelorji2013@gmail.com>
2023-07-16 09:37:17 +0100

    Use tailwind classes in Headline component

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -2,23 +2,26 @@ import React from 'react';
 import Socials from './Socials';
 
 const Headline = () => (
-  <section className="headline-container">
-    <div className="hero">
-      <h1>
-        <span>Hey There.</span>
-        <span>
-          I&#39;m Emmanuel
-        </span>
-      </h1>
-      <h2>I am a Software Developer</h2>
-      <p>
+  <section className="bg-customBlack px-6 py-[120px]">
+    <div className="">
+      <div className="w-fit">
+        <h1 className="font-creteRound text-customOrange text-[40px]/[4rem]">
+          Hi
+          <br />
+          <span className="block overflow-hidden whitespace-nowrap w-0 animate-typeAndClean border-r-4 border-customOrange">
+            I&#39;m Emmanuel
+          </span>
+        </h1>
+      </div>
+      <h2 className="text-customMilk my-3 text-2xl">I am a Software Developer</h2>
+      <p className="text-white text-base my-3">
         I can help you build a product, feature or website.
         Look through some of my work and experience!
         If you like what you see and have a project you need coded,
         don’t hesitate to contact me.
       </p>
     </div>
-    <Socials />
+    <Socials className="text-customLightGray flex py-2 gap-4 text-xl" />
   </section>
 );
 
