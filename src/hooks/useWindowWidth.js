76219f2e2816e1b0ac218291cc75c495125f09d4
emmiiorji <emmanuelorji2013@gmail.com>
2023-07-17 16:49:12 +0100

    Create custom hook to track window width

---
+++ src/hooks/useWindowWidth.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+
+const useWindowWidth = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // Add event listener to window resize
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener on unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return { windowWidth };
+};
+
+export default useWindowWidth;
