86673f76993bd43243aaaeb1a40693feee76bf08
emmiiorji <emmanuelorji2013@gmail.com>
2023-07-18 16:55:56 +0100

    Update the NextArrow component for testimonials slider

--- src/components/NextArrow.jsx
+++ src/components/NextArrow.jsx
@@ -2,26 +2,21 @@ import React from 'react';
 import { BiRightArrow } from 'react-icons/bi';
 import PropTypes from 'prop-types';
 
-const NextArrow = (props) => {
-  const { disabled, onClick } = props; // [disabled] is the same as props[0]
-  return (
-    <div
-      className="right-arrow slick-arrow"
-      role="presentation"
-      onClick={onClick}
-      style={{ background: disabled ? '#efefef' : '#97bf0f', cursor: disabled ? 'default' : 'pointer' }}
-    >
-      <BiRightArrow />
-    </div>
-  );
-};
+const NextArrow = ({ onClick }) => (
+  <button
+    type="button"
+    className="flex justify-center items-center bg-customOrange h-12 w-16 rounded-r-full cursor-pointer"
+    onClick={onClick}
+  >
+    <BiRightArrow className="text-3xl col" />
+  </button>
+);
 
 NextArrow.defaultProps = {
   onClick: () => {},
 };
 
 NextArrow.propTypes = {
-  disabled: PropTypes.bool.isRequired,
   onClick: PropTypes.func,
 };
 
