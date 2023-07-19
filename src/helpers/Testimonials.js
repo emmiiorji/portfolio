7decad355b3a243652bbab4d9446ccad0e6beb75
emmiiorji <emmanuelorji2013@gmail.com>
2023-07-19 17:27:39 +0100

    Add data for testimonials

---
+++ src/helpers/Testimonials.js
@@ -0,0 +1,28 @@
+const data = () => [
+  {
+    author: 'Ibrahim Lukman 1',
+    title: 'CTO at KSolutions Limited',
+    description: 'Emmanuel Orji-Ihuoma is a gifted Software Engineer. The thought process he puts into his codes is exceptional. Thank you for everything Emmanuel Orji-Ihuoma',
+    headshot: 'https://res.cloudinary.com/emmii/image/upload/v1689921515/my_portfolio/Mr_Kenny_dctyvk.jpg',
+  },
+  {
+    author: 'Ibrahim Lukman 2',
+    title: 'CTO at KSolutions Limited',
+    description: 'Emmanuel Orji-Ihuoma is a gifted Software Engineer. The thought process he puts into his codes is exceptional. Thank you for everything Emmanuel Orji-Ihuoma',
+    headshot: 'https://res.cloudinary.com/emmii/image/upload/v1689921515/my_portfolio/Mr_Kenny_dctyvk.jpg',
+  },
+  {
+    author: 'Ibrahim Lukman 3',
+    title: 'CTO at KSolutions Limited',
+    description: 'Emmanuel Orji-Ihuoma is a gifted Software Engineer. The thought process he puts into his codes is exceptional. Thank you for everything Emmanuel Orji-Ihuoma',
+    headshot: 'https://res.cloudinary.com/emmii/image/upload/v1689921515/my_portfolio/Mr_Kenny_dctyvk.jpg',
+  },
+  {
+    author: 'Ibrahim Lukman 4',
+    title: 'CTO at KSolutions Limited',
+    description: 'Emmanuel Orji-Ihuoma is a gifted Software Engineer. The thought process he puts into his codes is exceptional. Thank you for everything Emmanuel Orji-Ihuoma',
+    headshot: 'https://res.cloudinary.com/emmii/image/upload/v1689921515/my_portfolio/Mr_Kenny_dctyvk.jpg',
+  },
+];
+
+export default data();
