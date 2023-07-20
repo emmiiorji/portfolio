ba76fb26882aa1ece1c49a4f7d243ed1c6053a5e
emmiiorji <emmanuelorji2013@gmail.com>
2023-07-20 20:53:04 +0100

    Update carousel Next Arrow component

--- src/components/NextArrow.jsx
+++ src/components/NextArrow.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { BiRightArrow } from 'react-icons/bi';
 import PropTypes from 'prop-types';
 
-const NextArrow = ({ onClick }) => (
-  <button
-    type="button"
-    className="flex justify-center items-center bg-customOrange h-12 w-16 rounded-r-full cursor-pointer"
-    onClick={onClick}
-  >
-    <BiRightArrow className="text-3xl col" />
-  </button>
+const NextArrow = ({ onClick, lastSlide }) => (
+  <div className="absolute top-0 right-0 h-full flex items-center">
+    <button
+      type="button"
+      className={`flex justify-center items-center ${lastSlide ? 'bg-customLighterGray' : 'bg-customOrange'} h-12 w-16 rounded-r-full cursor-pointer`}
+      onClick={onClick}
+    >
+      <BiRightArrow className="text-white text-3xl col" />
+    </button>
+  </div>
 );
 
 NextArrow.defaultProps = {
@@ -18,6 +20,7 @@ NextArrow.defaultProps = {
 
 NextArrow.propTypes = {
   onClick: PropTypes.func,
+  lastSlide: PropTypes.bool.isRequired,
 };
 
 export default NextArrow;
