2357474d045c1430c902925a2e3253ada10fb888
emmiiorji <emmanuelorji2013@gmail.com>
2023-07-20 20:52:31 +0100

    Create component for carousel dot

---
+++ src/components/Dots.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { nanoid } from 'nanoid';
+
+const Dots = ({ dots, currentSlide }) => (
+  <ul className="flex justify-center list-none p-0 mt-4">
+    {
+      dots.map((dot, index) => (
+        <li
+          key={nanoid()}
+          className={`mx-2 w-4 h-4 rounded-full ${
+            index === currentSlide ? 'bg-blue-500' : 'bg-gray-300'
+          }`}
+        >
+          <button type="button" className="flex items-center justify-center h-full w-full" onClick={dot.props.children.props.onClick}>&nbsp;</button>
+        </li>
+      ))
+    }
+  </ul>
+);
+
+Dots.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  dots: PropTypes.arrayOf(PropTypes.object).isRequired,
+  currentSlide: PropTypes.number.isRequired,
+};
+
+export default Dots;
