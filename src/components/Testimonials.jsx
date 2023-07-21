a6f4e8a225e29e29da4976d23e57567e2086792c
emmiiorji <emmanuelorji2013@gmail.com>
2023-07-22 00:41:15 +0100

    Update Testimonials component

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -8,26 +8,35 @@ import NextArrow from './NextArrow';
 import testimonials from '../helpers/Testimonials';
 import TestimonialCard from './TestimonialCard';
 import useWindowWidth from '../hooks/useWindowWidth';
+import Dots from './Dots';
 
 const Testimonials = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const { windowWidth } = useWindowWidth();
+  const numOfSlides = windowWidth < 768 ? 1 : 3;
+
   return (
-    <section className="flex flex-col items-center mt-30">
+    <section className="flex flex-col items-center" id="slick">
       <Slider
-        prevArrow={<PrevArrow />} // onClick prop is passed in by react-slick
-        nextArrow={<NextArrow />}
-        beforeChange={(current, next) => setCurrentSlide(next, currentSlide)}
-        className="w-full bg-white"
-        slidesToShow={windowWidth < 768 ? 1 : 3}
+        prevArrow={<PrevArrow firstSlide={currentSlide === 0} />}
+        nextArrow={<NextArrow lastSlide={currentSlide === testimonials.length - numOfSlides} />}
+        speed={1000}
+        dots
+        appendDots={(dots) => <Dots dots={dots} currentSlide={currentSlide} />}
+        beforeChange={(current, next) => setCurrentSlide(next)}
+        className="w-full max-w-3xl self-center bg-customLightBlack rounded-lg px-10 md:px-0 py-4 mt-5"
+        slidesToShow={numOfSlides}
       >
-        {testimonials.map((testimonial) => (
+        {testimonials.map((testimonial, index) => (
           <TestimonialCard
             key={nanoid()}
             author={testimonial.author}
             headshot={testimonial.headshot}
             title={testimonial.title}
             description={testimonial.description}
+            isCurrentSlide={
+              index === (currentSlide + testimonials.length + 1) % testimonials.length
+            }
           />
         ))}
       </Slider>
