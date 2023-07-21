b515424018d48399da898d395f3b63570ef83b59
emmiiorji <emmanuelorji2013@gmail.com>
2023-07-21 20:53:48 +0100

    Update carousel Prev Arrow component

--- src/components/PrevArrow.jsx
+++ src/components/PrevArrow.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { BiLeftArrow } from 'react-icons/bi';
 import PropTypes from 'prop-types';
 
-const PrevArrow = ({ onClick }) => (
-  <button
-    type="button"
-    className="flex justify-center items-center bg-customOrange h-12 w-16 rounded-l-full cursor-pointer"
-    onClick={onClick}
-  >
-    <BiLeftArrow className="text-3xl col" />
-  </button>
+const PrevArrow = ({ onClick, firstSlide }) => (
+  <div className="absolute top-0 left-0 z-10 h-full flex items-center">
+    <button
+      type="button"
+      className={`flex justify-center items-center ${firstSlide ? 'bg-customLighterGray' : 'bg-customOrange'} h-12 w-16 rounded-l-full cursor-pointer`}
+      onClick={onClick}
+    >
+      <BiLeftArrow className="text-white text-3xl col" />
+    </button>
+  </div>
 );
 
 PrevArrow.defaultProps = {
@@ -18,6 +20,7 @@ PrevArrow.defaultProps = {
 
 PrevArrow.propTypes = {
   onClick: PropTypes.func,
+  firstSlide: PropTypes.bool.isRequired,
 };
 
 export default PrevArrow;
