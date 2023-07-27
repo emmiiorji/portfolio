4f956372356d0f5dae071a0f8763c485596a0a93
emmiiorji <emmanuelorji2013@gmail.com>
2023-07-27 16:55:48 +0100

    Add helper method to convert keyframe object into css string

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -23,9 +23,15 @@ const scrollToBottom = () => {
 const getStyleProperties = (styleObject) => Object.entries(styleObject)
   .map(([prop, value]) => `${prop}: ${value}`).join('; ');
 
+// Helper function to convert the keyframes object into a CSS string
+const getKeyframeString = (keyframes, keyframeName) => `@keyframes ${keyframeName} {
+  ${Object.entries(keyframes).map(([key, value]) => `${key} { ${getStyleProperties(value)} }`).join('\n')}
+}`;
+
 export {
   scrollToSection,
   scrollToTop,
   scrollToBottom,
   getStyleProperties,
+  getKeyframeString,
 };
