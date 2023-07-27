c86fdbf09cbf38756af4119c8eaaf265e35751b1
emmiiorji <emmanuelorji2013@gmail.com>
2023-07-27 18:42:20 +0100

    Update Headline component

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -1,16 +1,33 @@
-import React, { createRef } from 'react';
+import React, { useEffect } from 'react';
 import Socials from './Socials';
 import headShot from '../assets/img/IMG_3525.jpg';
 import Testimonials from './Testimonials';
+import {
+  blinking,
+  cursor, typeHeadlineName, typeHeadlineTitle, typing,
+} from '../helpers/dynamicKeyframes';
 
 const Headline = () => {
-  const headlineParaRef = createRef();
   const headlineText = 'I can help you build a product, feature or website. Look through some of my work and experience! If you like what you see and have a project you need coded, don’t hesitate to contact me.';
-  const headlineStyle = {
-    // Currently, headlineText is 186 characters. When modified recalculate animations and keyframes
-    backgroundSize: `${headlineText.length * 1}ch 200%`,
-    animation: `cursor .7s infinite steps(1), typing ${headlineText.length * 0.3}s steps(${headlineText.length}) infinite forwards`,
-  };
+  const paragraphTime = headlineText.length * 0.2;
+  const totalTime = paragraphTime + 25;
+
+  useEffect(() => {
+    const styleElement = document.createElement('style');
+    styleElement.appendChild(document.createTextNode(
+      typeHeadlineName(totalTime)
+      + typeHeadlineTitle(totalTime)
+      + cursor()
+      + typing(headlineText.length, totalTime, paragraphTime)
+      + blinking(),
+    ));
+    document.head.appendChild(styleElement);
+
+    return () => {
+      // Clean up the style element when the component unmounts
+      document.head.removeChild(styleElement);
+    };
+  });
 
   return (
     <section className="bg-customBlack bg-no-repeat bg-right-bottom md:bg-right px-6 pt-[20px] pb-[120px]">
@@ -19,26 +36,42 @@ const Headline = () => {
           <img src={headShot} alt="headshot" className="w-full max-w-xs rounded-2xl border-4 border-customRed" />
         </div>
         <div className="flex flex-col max-w-full">
-          <h1 className="font-creteRound w-fit text-customOrange text-[40px]/[4rem] md:text-[72px]/[82px]">
-            Hi,
-            <span className="block overflow-hidden whitespace-nowrap w-0 animate-typeHeadlineName border-r-4 border-customOrange">
-              I&#39;m Emmanuel
-            </span>
-          </h1>
-          <h2 className="max-w-xs w-fit">
-            <span className="animate-typeHeadlineTitle border-r-4 whitespace-nowrap border-customMilk w-0 overflow-hidden text-customMilk my-3 text-2xl font-roboto">I am a Software Developer</span>
-          </h2>
-          <p className="text-white max-w-sm text-xl my-3 font-poppins">
-            <span
-              ref={headlineParaRef}
-              className="border-customOrange"
-              id="headlineParagraph"
-              style={headlineStyle}
-            >
-              {headlineText}
-            </span>
-          </p>
-          <Socials className="text-customLightGray flex py-2 gap-4 text-xl" />
+          <div className="h-80 relative">
+            <h1 className="absolute mb-16 left-0 font-creteRound w-fit text-customOrange text-[40px]/[4rem] md:text-[72px]/[82px]">
+              <span>Hi,</span>
+              <span
+                style={{
+                  animation: `typeHeadlineName ${totalTime}s steps(50, end) infinite, blinking 1s infinite`,
+                }}
+                className="block overflow-hidden whitespace-nowrap w-0 border-r-4 border-customOrange"
+              >
+                I&#39;m Emmanuel
+              </span>
+            </h1>
+            <div className="absolute top-16 left-0 max-w-xs w-fit">
+              <h2
+                style={{
+                  animation: `typeHeadlineTitle ${totalTime}s steps(50, end) infinite, blinking 1s infinite`,
+                }}
+                className="border-r-4 whitespace-nowrap border-customMilk w-0 overflow-hidden text-customMilk my-3 text-2xl font-roboto"
+              >
+                I am a Software Developer
+              </h2>
+            </div>
+            <p className="absolute top-16 left-0 text-white md:max-w-sm text-xl my-3 font-poppins">
+              <span
+                className="font-bold border-customOrange"
+                id="headlineParagraph"
+                style={{
+                  backgroundSize: `${headlineText.length * 1}ch 200%`,
+                  animation: `cursor .7s infinite steps(1), typing ${totalTime}s steps(${headlineText.length}) infinite forwards`,
+                }}
+              >
+                {headlineText}
+              </span>
+            </p>
+          </div>
+          <Socials className="text-customLightGray flex md:flex-col md:-ml-[8vw] py-2 gap-4 text-xl" />
         </div>
       </div>
       <Testimonials />
