02a1cc9af64de9b0df95230297b9e9edeaefa342
emmiiorji <emmanuelorji2013@gmail.com>
2023-07-29 20:32:31 +0100

    Update Testimonials component

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -13,21 +13,23 @@ import Dots from './Dots';
 const Testimonials = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const { windowWidth } = useWindowWidth();
-  const numOfSlides = windowWidth < 768 ? 1 : 3;
+  const numOfSlides = 1;
 
   return (
-    <section className="flex flex-col items-center mt-32 px-[calc(10.416%-24px)] md:p-0" id="slick">
+    <section className="flex flex-col items-center mt-16 px-[calc(10.416%-24px)] md:p-0" id="slick">
       <h2 className="text-3xl font-bold text-customOffWhite mb-6 text-center">Hear what they say:</h2>
-      <div className="drop-shadow-customLg w-full max-w-3xl bg-customLightBlack px-10 md:px-4 tab:px-10 md:box-content rounded-lg">
+      <div className="drop-shadow-customLg w-full max-w-2xl bg-customLightBlack px-10 md:px-4 tab:px-10 md:box-content rounded-lg">
         <Slider
           prevArrow={<PrevArrow />}
           nextArrow={<NextArrow />}
           speed={1000}
           dots
+          centerMode={windowWidth > 768}
           appendDots={(dots) => <Dots dots={dots} currentSlide={currentSlide} />}
           beforeChange={(current, next) => setCurrentSlide(next)}
           className="z-10 py-4"
           slidesToShow={numOfSlides}
+          centerPadding="20%"
           autoplay
           autoplaySpeed={6000}
         >
@@ -39,7 +41,7 @@ const Testimonials = () => {
               title={testimonial.title}
               description={testimonial.description}
               isCurrentSlide={
-                index === (currentSlide + testimonials.length + 1) % testimonials.length
+                index === (currentSlide + testimonials.length) % testimonials.length
               }
             />
           ))}
