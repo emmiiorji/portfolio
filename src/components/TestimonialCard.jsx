4c50450e8f029953ff5970a04b401727d4d5fc4d
emmiiorji <emmanuelorji2013@gmail.com>
2023-07-30 05:18:47 +0100

    Update Testimonial Card component

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -7,19 +7,32 @@ const TestimonialCard = ({
 }) => {
   const { isMobile } = useWindowWidth();
   return (
-    <div className="relative h-full bg-white rounded-xl my-4">
+    <div className="relative h-[350px] xssm:h-80 ssm:h-64 bg-white rounded-xl my-4">
       <div className={`${isCurrentSlide || isMobile ? '' : 'absolute top-0 left-0 h-full w-full rounded-xl bg-whiteOverlay'}`} />
-      <div className={`${isCurrentSlide ? '' : ''} flex flex-col p-4 text-center items-center my-5 rounded-lg`}>
-        <div className="flex flex-col justify-between items-center">
-          <div className="self-center justify-center rounded-full border-2 box-content border-customRed truncate h-10 w-10">
-            <img className="" src={headshot} alt={`${author}'s headshot`} />
-          </div>
-          <h2 className="text-base">{author}</h2>
-          <small className="text-xs">{title}</small>
+      <div className="flex flex-col h-full items-center justify-center p-4">
+        <div className="flex mt-5">
+          <div className="w-7 h-0.5 bg-customRed" />
+          <p className="-mt-10 mx-2 text-customRed font-coustard rotate-180 text-5xl">&quot;</p>
+          <div className="w-28 xssm:36 h-0.5 bg-customRed" />
         </div>
-        <p className="text-base/5 mt-2 whitespace-pre-wrap">
-          {description.length > 160 ? `${description.slice(0, 160)}....` : description}
-        </p>
+        <div
+          className="flex flex-col items-center xssm:items-start xssm:flex-row gap-4 p-2 text-left rounded-lg"
+          style={{ background: '#f3f3f3' }}
+        >
+          <img className="border-customRed border-2 rounded-full w-12 h-12" src={headshot} alt={`${author}'s headshot`} />
+          <div className="flex flex-col items-start">
+            <h2 className="font-bold text-base self-center xssm:self-start">{author}</h2>
+            <small className="text-xs self-center xssm:self-start">{title}</small>
+            <p className="text-base/5 mt-2 whitespace-pre-wrap">
+              {description.length > 160 ? `${description.slice(0, 160)}....` : description}
+            </p>
+          </div>
+        </div>
+        <div className="flex mt-2 h-5">
+          <div className="w-28 xssm:36 h-[2px] bg-customRed" />
+          <p className="-mt-2 mx-2 text-customRed font-coustard text-5xl">&quot;</p>
+          <div className="w-7 h-0.5 bg-customRed" />
+        </div>
       </div>
     </div>
   );
