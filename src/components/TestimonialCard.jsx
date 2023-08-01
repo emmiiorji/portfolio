32253c1ed4b7c6cec508dea5102dedbdbd6d23c0
emmiiorji <emmanuelorji2013@gmail.com>
2023-08-01 09:29:15 +0100

    Use QuoteLine component in  Testimonial Card component

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import useWindowWidth from '../hooks/useWindowWidth';
+import QuoteLine from './QuoteLine';
 
 const TestimonialCard = ({
   author, headshot, title, description, isCurrentSlide,
@@ -10,11 +11,7 @@ const TestimonialCard = ({
     <div className="relative h-[350px] xssm:h-80 ssm:h-64 bg-white rounded-xl my-4">
       <div className={`${isCurrentSlide || isMobile ? '' : 'absolute top-0 left-0 h-full w-full rounded-xl bg-whiteOverlay'}`} />
       <div className="flex flex-col h-full items-center justify-center p-4">
-        <div className="flex mt-5">
-          <div className="w-7 h-0.5 bg-customRed" />
-          <p className="-mt-10 mx-2 text-customRed font-coustard rotate-180 text-5xl">&quot;</p>
-          <div className="w-28 xssm:36 h-0.5 bg-customRed" />
-        </div>
+        <QuoteLine className="rotate-180 mt-0 mb-2 h-6" />
         <div
           className="flex flex-col items-center xssm:items-start xssm:flex-row gap-4 p-2 text-left rounded-lg"
           style={{ background: '#f3f3f3' }}
@@ -28,11 +25,7 @@ const TestimonialCard = ({
             </p>
           </div>
         </div>
-        <div className="flex mt-2 h-5">
-          <div className="w-28 xssm:36 h-[2px] bg-customRed" />
-          <p className="-mt-2 mx-2 text-customRed font-coustard text-5xl">&quot;</p>
-          <div className="w-7 h-0.5 bg-customRed" />
-        </div>
+        <QuoteLine className="" />
       </div>
     </div>
   );
