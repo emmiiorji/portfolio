b4b8119f2ca7d609a40bbb10f232184c715285d3
emmiiorji <emmanuelorji2013@gmail.com>
2023-08-06 03:40:30 +0100

    Update testimonials array

--- src/helpers/Testimonials.js
+++ src/helpers/Testimonials.js
@@ -1,6 +1,6 @@
 const data = () => [
   {
-    author: 'Ibrahim Lukman 1',
+    author: 'Ibrahim Lukman',
     title: 'CTO at KSolutions Limited',
     description: 'Emmanuel Orji-Ihuoma is a gifted Software Engineer. The thought process he puts into his codes is exceptional. Thank you for everything Emmanuel Orji-Ihuoma.',
     headshot: 'https://res.cloudinary.com/emmii/image/upload/v1689921515/my_portfolio/Mr_Kenny_dctyvk.jpg',
@@ -13,27 +13,27 @@ const data = () => [
   },
   {
     author: 'Raymond Olisa',
-    title: 'Full-stack Developer',
+    title: 'Full-stack Developer, Abuja, Nigeria',
     description: 'I highly recommend Emmanuel Orji-Ihuoma as a full-stack developer for his exceptional skills and unwavering commitment to assisting others. His in-depth knowledge of both front-end and back-end development, coupled with his passion for coding, makes him an invaluable resource for any project.',
     headshot: 'https://res.cloudinary.com/emmii/image/upload/v1691502392/my_portfolio/RaymondOlisa_400x400_l7tche.jpg',
   },
   {
-    author: 'Ibrahim Lukman 4',
-    title: 'CTO at KSolutions Limited',
-    description: 'Emmanuel Orji-Ihuoma is a gifted Software Engineer. The thought process he puts into his codes is exceptional. Thank you for everything Emmanuel Orji-Ihuoma.',
-    headshot: 'https://res.cloudinary.com/emmii/image/upload/v1689921515/my_portfolio/Mr_Kenny_dctyvk.jpg',
+    author: 'Chris Clothier',
+    title: 'Full-stack Developer, UK',
+    description: 'Emmanuel is a great software developer who is dedicated to the job at hand. His collaboration and problem solving skills are second to none. He learns quickly and put great effort toward achieving his goals as a software developer.',
+    headshot: 'https://res.cloudinary.com/emmii/image/upload/v1691546963/my_portfolio/ChrisClotier_khupxs.jpg',
   },
   {
-    author: 'Ibrahim Lukman 5',
-    title: 'CTO at KSolutions Limited',
-    description: 'Emmanuel Orji-Ihuoma is a gifted Software Engineer. The thought process he puts into his codes is exceptional. Thank you for everything Emmanuel Orji-Ihuoma.',
-    headshot: 'https://res.cloudinary.com/emmii/image/upload/v1689921515/my_portfolio/Mr_Kenny_dctyvk.jpg',
+    author: 'Tosin Ogundana',
+    title: 'Data Analyst, Sheffield, UK',
+    description: 'Working with you on a machine learning project was interesting; you were encouraging and goal-oriented.',
+    headshot: 'https://res.cloudinary.com/emmii/image/upload/v1691547170/my_portfolio/Tosin_Ogundana_mv0qdh.jpg',
   },
   {
-    author: 'Ibrahim Lukman 6',
-    title: 'CTO at KSolutions Limited',
-    description: 'Emmanuel Orji-Ihuoma is a gifted Software Engineer. The thought process he puts into his codes is exceptional. Thank you for everything Emmanuel Orji-Ihuoma.',
-    headshot: 'https://res.cloudinary.com/emmii/image/upload/v1689921515/my_portfolio/Mr_Kenny_dctyvk.jpg',
+    author: 'Sunday Anthony Ezema',
+    title: 'Full-stack Developer, Lagos, Nigeria',
+    description: 'Emmanuel is an intelligent young man. He is passion driven, working with him as my coding partner, really made me to learn a lot of things. I recommend him any position for any role related to his field/skills.',
+    headshot: 'https://res.cloudinary.com/emmii/image/upload/v1691547416/my_portfolio/EzemaAnthony_thslly.jpg',
   },
 ];
 
