fc6d573d5098b446ba7908e90803f79d7e895bf9
emmiiorji <emmanuelorji2013@gmail.com>
2023-08-08 02:23:54 +0100

    Update Testimonials array

--- src/helpers/Testimonials.js
+++ src/helpers/Testimonials.js
@@ -7,15 +7,15 @@ const data = () => [
   },
   {
     author: 'Ntihinduka Alpha',
-    title: 'Full Stack Developer, Code Reviewer at Microverse',
+    title: 'Full-stack Developer, Code Reviewer at Microverse',
     description: 'I highly recommend Emmanuel as a valuable addition to any team. His skills make him the best choice for anyone looking to strengthen their team.',
     headshot: 'https://res.cloudinary.com/emmii/image/upload/v1691306411/my_portfolio/1656626641110_c9c5og.jpg',
   },
   {
-    author: 'Ibrahim Lukman 3',
-    title: 'CTO at KSolutions Limited',
-    description: 'Emmanuel Orji-Ihuoma is a gifted Software Engineer. The thought process he puts into his codes is exceptional. Thank you for everything Emmanuel Orji-Ihuoma.',
-    headshot: 'https://res.cloudinary.com/emmii/image/upload/v1689921515/my_portfolio/Mr_Kenny_dctyvk.jpg',
+    author: 'Raymond Olisa',
+    title: 'Full-stack Developer',
+    description: 'I highly recommend Emmanuel Orji-Ihuoma as a full-stack developer for his exceptional skills and unwavering commitment to assisting others. His in-depth knowledge of both front-end and back-end development, coupled with his passion for coding, makes him an invaluable resource for any project.',
+    headshot: 'https://res.cloudinary.com/emmii/image/upload/v1691502392/my_portfolio/RaymondOlisa_400x400_l7tche.jpg',
   },
   {
     author: 'Ibrahim Lukman 4',
