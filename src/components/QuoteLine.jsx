06907fb5864dd2a016917e5c49c57b20bea45df6
emmiiorji <emmanuelorji2013@gmail.com>
2023-08-08 02:22:28 +0100

    Update QuoteLine component

--- src/components/QuoteLine.jsx
+++ src/components/QuoteLine.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const QuoteLine = ({ className }) => (
-  <div className={`flex mt-2 h-5 ${className}`}>
+  <div className={`flex my-2 h-5 ${className}`}>
     <div className="w-28 xssm:36 h-[2px] bg-customRed" />
     <p className="-mt-2 mx-2 text-customRed font-coustard text-5xl">&quot;</p>
     <div className="w-7 h-0.5 bg-customRed" />
