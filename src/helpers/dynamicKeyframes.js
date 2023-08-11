2f40ca2567b96bcd897954402a8d178f7b019dee
emmiiorji <emmanuelorji2013@gmail.com>
2023-08-11 23:59:08 +0100

    Reorder methods in dynamic keyframes

--- src/helpers/dynamicKeyframes.js
+++ src/helpers/dynamicKeyframes.js
@@ -1,36 +1,5 @@
 import { getKeyframeString } from '.';
 
-const typing = (n, totalTime) => {
-  const keyframeObj = {
-    '0%': {
-      visibility: 'hidden',
-      // display: 'none',
-      'background-size': '0 200%',
-    },
-    [`${(25 / totalTime) * 100}%, 100%`]: {
-      'background-size': '0 200%',
-      visibility: 'visible',
-      // display: 'inline',
-    },
-    [`${((totalTime - 10) / totalTime) * 100}%`]: {
-      'background-size': `calc(${n * 1}ch) 200%`,
-    },
-  };
-  return getKeyframeString(keyframeObj, 'typing');
-};
-
-const cursor = () => {
-  const keyframeObj = {
-    '0%, 100%': {
-      'background-position': '0 0, 0 0',
-    },
-    '50%': {
-      'background-position': '0 -100%, 0 0',
-    },
-  };
-  return getKeyframeString(keyframeObj, 'cursor');
-};
-
 const typeHeadlineName = (totalTime) => {
   const keyframeObj = {
     [`0%, ${(2 / totalTime) * 100}%, ${(12 / totalTime) * 100}%`]: {
@@ -66,6 +35,37 @@ const typeHeadlineTitle = (totalTime) => {
     ,
   };
   return getKeyframeString(keyframeObj, 'typeHeadlineTitle');
+};
+
+const typing = (n, totalTime) => {
+  const keyframeObj = {
+    '0%': {
+      visibility: 'hidden',
+      // display: 'none',
+      'background-size': '0 200%',
+    },
+    [`${(25 / totalTime) * 100}%, 100%`]: {
+      'background-size': '0 200%',
+      visibility: 'visible',
+      // display: 'inline',
+    },
+    [`${((totalTime - 10) / totalTime) * 100}%`]: {
+      'background-size': `calc(${n * 1}ch) 200%`,
+    },
+  };
+  return getKeyframeString(keyframeObj, 'typing');
+};
+
+const cursor = () => {
+  const keyframeObj = {
+    '0%, 100%': {
+      'background-position': '0 0, 0 0',
+    },
+    '50%': {
+      'background-position': '0 -100%, 0 0',
+    },
+  };
+  return getKeyframeString(keyframeObj, 'cursor');
 };
 
 const blinking = () => {
