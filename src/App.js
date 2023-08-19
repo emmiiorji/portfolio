cf38e27b14d90075ec09ecc19fe7c1b3947910e3
emmiiorji <emmanuelorji2013@gmail.com>
2023-08-19 15:31:31 +0100

    Update router in App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter, RouterProvider, useRouteError, isRouteErrorResponse,
+} from 'react-router-dom';
 import Root from './pages/root';
 import NotFound from './pages/notFound';
 import Home from './pages/home';
 
 const App = () => {
+  const ErrorBoundary = () => {
+    const error = useRouteError();
+    return (
+      (isRouteErrorResponse(error) && error.status === 404)
+        ? <NotFound /> : <div>Oops! Something went wrong.</div>
+    );
+  };
+
   const router = createBrowserRouter([
     {
       path: '/',
       element: <Root />,
-      errorElement: <NotFound />,
+      errorElement: <ErrorBoundary />,
       children: [
         {
           path: '/',
