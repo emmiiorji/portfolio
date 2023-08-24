7cccb9280ad44db5028c955607d00b607cc8c78a
emmiiorji <emmanuelorji2013@gmail.com>
2023-08-24 17:18:21 +0100

    Update Header component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -8,6 +8,7 @@ const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [menuClosing, setMenuClosing] = useState(false);
   const { isMobile } = useWindowWidth();
+  const toShowMenu = menuOpen && isMobile;
 
   const toggleMenu = (event, menuClosing) => {
     if (menuClosing) {
@@ -22,33 +23,32 @@ const Header = () => {
   return (
     <header className="font-roboto">
       <nav
-        className={`flex ${`${menuClosing ? 'animate-slideOut' : ''}`} ${menuOpen ? 'animate-slideIn flex-col h-screen w-screen fixed z-10' : 'justify-between px-3'} bg-customGrey text-white pr-0 md:items-center md:px-[10.416%]`}
+        className={`flex ${menuClosing ? 'animate-slideOut' : ''} ${toShowMenu ? 'animate-slideIn flex-col h-screen w-screen fixed z-10' : 'justify-between px-3'} bg-customGrey text-white pr-0 md:items-center md:px-[10.416%]`}
       >
-        <a className={`self-center ${menuOpen ? 'hidden' : ''}`} href="/" id="author-name">Emmanuel Orji</a>
-        <div className={`p-4 flex items-center ${menuOpen ? 'self-end ' : ''}`} id="mobile-menu">
+        <a className={`self-center ${toShowMenu ? 'hidden' : ''}`} href="/" id="author-name">Emmanuel Orji</a>
+        <div className={`p-4 flex items-center ${toShowMenu ? 'self-end' : ''}`} id="mobile-menu">
           <GiHamburgerMenu
             className={`${!menuOpen && isMobile ? '' : 'hidden'}`}
             onClick={toggleMenu}
           />
           <AiOutlineClose
-            className={`${!menuOpen ? 'hidden' : ''}`}
+            className={`${!toShowMenu ? 'hidden' : ''}`}
             onClick={(event) => toggleMenu(event, true)}
           />
         </div>
         <ul
-          className={`flex md:pr-0 ${menuOpen ? 'text-customMilk text-3xl/[44px] font-bold flex-col px-6' : `${isMobile ? 'hidden' : 'font-semibold items-center p-2'}`}`}
-          id="nav-menu"
+          className={`flex md:pr-0 ${toShowMenu ? 'text-customMilk text-3xl/[44px] font-bold flex-col px-6' : `${isMobile ? 'hidden' : 'font-semibold items-center p-2'}`}`}
         >
-          <li className={`${menuOpen ? 'border-b border-customLighterGray' : ''} hover:bg-customOrange hover:rounded p-2`}>
+          <li className={`${toShowMenu ? 'border-b border-customLighterGray' : ''} hover:bg-customOrange hover:rounded p-2`}>
             <a href="#my-works" onClick={(event) => toggleMenu(event, menuOpen)}>Portfolio</a>
           </li>
-          <li className={`${menuOpen ? 'border-b border-customLighterGray' : ''} hover:bg-customOrange hover:rounded p-2`}>
+          <li className={`${toShowMenu ? 'border-b border-customLighterGray' : ''} hover:bg-customOrange hover:rounded p-2`}>
             <a href="#about-me" onClick={(event) => toggleMenu(event, menuOpen)}>About</a>
           </li>
-          <li className={`${menuOpen ? 'border-b border-customLighterGray' : ''} hover:bg-customOrange hover:rounded p-2`}>
+          <li className={`${toShowMenu ? 'border-b border-customLighterGray' : ''} hover:bg-customOrange hover:rounded p-2`}>
             <a href="#contact-me" onClick={(event) => toggleMenu(event, menuOpen)}>Contact</a>
           </li>
-          <li className={`${menuOpen ? 'hidden' : ''} hover:bg-customOrange hover:rounded p-2.5`}><BsFillEnvelopeFill /></li>
+          <li className={`${toShowMenu ? 'hidden' : ''} hover:bg-customOrange hover:rounded p-2.5`}><BsFillEnvelopeFill /></li>
         </ul>
       </nav>
     </header>
