dd7d416924cb809b069ebc8eb28eeafe9145cf30
emmiiorji <emmanuelorji2013@gmail.com>
2023-08-28 12:49:26 +0100

    Adjust testimonial comments

--- src/helpers/testimonials/index.js
+++ src/helpers/testimonials/index.js
@@ -2,7 +2,7 @@ const data = () => [
   {
     author: 'Ibrahim Lukman',
     title: 'CTO at KSolutions Limited',
-    description: 'Emmanuel Orji-Ihuoma is a gifted Software Engineer. The thought process he puts into his codes is exceptional. Thank you for everything Emmanuel Orji-Ihuoma.',
+    description: 'He is a gifted Software Engineer. The thought process he puts into his codes is exceptional.',
     headshot: 'https://res.cloudinary.com/emmii/image/upload/v1689921515/my_portfolio/Mr_Kenny_dctyvk.jpg',
   },
   {
@@ -14,25 +14,25 @@ const data = () => [
   {
     author: 'Chris Clothier',
     title: 'Full-stack Developer, UK',
-    description: 'Emmanuel is a great software developer who is dedicated to the job at hand. His collaboration and problem solving skills are second to none. He learns quickly and put great effort toward achieving his goals as a software developer.',
+    description: 'Emmanuel is a great software developer who is dedicated to the job. His collaboration and problem solving skills are second to none.',
     headshot: 'https://res.cloudinary.com/emmii/image/upload/v1691546963/my_portfolio/ChrisClotier_khupxs.jpg',
   },
   {
     author: 'Tosin Ogundana',
     title: 'Data Analyst, Sheffield, UK',
-    description: 'Working with you on a machine learning project was interesting; you were encouraging and goal-oriented.',
+    description: 'Working with him on a machine learning project was interesting; he was encouraging and goal-oriented.',
     headshot: 'https://res.cloudinary.com/emmii/image/upload/v1691547170/my_portfolio/Tosin_Ogundana_mv0qdh.jpg',
   },
   {
     author: 'Raymond Olisa',
     title: 'Full-stack Developer, Abuja, Nigeria',
-    description: 'I highly recommend Emmanuel Orji-Ihuoma as a full-stack developer for his exceptional skills and unwavering commitment to assisting others. His in-depth knowledge of both front-end and back-end development, coupled with his passion for coding, makes him an invaluable resource for any project.',
+    description: 'Emmanuel has exceptional skills and unwavering commitment to assisting others.',
     headshot: 'https://res.cloudinary.com/emmii/image/upload/v1691502392/my_portfolio/RaymondOlisa_400x400_l7tche.jpg',
   },
   {
     author: 'Sunday Anthony Ezema',
     title: 'Full-stack Developer, Lagos, Nigeria',
-    description: 'Emmanuel is an intelligent young man. He is passion driven, working with him as my coding partner, really made me to learn a lot of things. I recommend him any position for any role related to his field/skills.',
+    description: 'He is an intelligent young man and passion driven. Working with him as my coding partner, really made me to learn a lot. I recommend him.',
     headshot: 'https://res.cloudinary.com/emmii/image/upload/v1691547416/my_portfolio/EzemaAnthony_thslly.jpg',
   },
 ];
